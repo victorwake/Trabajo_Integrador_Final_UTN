cbd6b5aa07ea47647981d720e1af7882b896734e
Victor Pinto <victorpintowake@gmail.com>
2023-04-21 00:00:46 -0300

    maquetado

---
+++ TPI_UTN/src/main/java/org/example/conexion/db/ConectorSQL.java
@@ -0,0 +1,16 @@
+package org.example.conexion.db;
+
+public class ConectorSQL {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/prueba";
+
+    // Credenciales credentials
+    public static final String USER = "root";
+    public static final String PASS = "root";
+
+    public ConectorSQL() {
+    }
+
+
+}
